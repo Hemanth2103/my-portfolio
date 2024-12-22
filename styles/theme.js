76db253fa359bff4ac02545f6ed99540a4fffc9d
Hemanth Reddy Sudini <sudinihemanth@gmail.com>
2024-12-21 20:19:48 -0700

    Initial Styles

---
+++ styles/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+    colors: {
+      red: '#F27474', 
+      green: '#66BB6A', 
+      blue: '#2196F3', 
+      black: '#141414', 
+      white: '#FFFFFF', 
+      gray: '#808080', 
+    },
+    fonts: {
+      primary: 'Roboto, sans-serif', 
+    },
+  };
+  
+  export default theme; 
